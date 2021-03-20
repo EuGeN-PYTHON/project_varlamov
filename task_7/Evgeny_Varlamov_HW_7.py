89c392cefc238cf65221daa74306e049c12a142d
EuGeN-PYTHON <varlaea2@icloud.com>
2021-03-20 23:02:48 +0300

    update_HW_7(2 task)

--- task_7/Evgeny_Varlamov_HW_7.py
+++ task_7/Evgeny_Varlamov_HW_7.py
@@ -3,9 +3,6 @@ from collections import defaultdict
 from os.path import relpath
 from distutils.dir_util import copy_tree
 
-import yaml
-from pprint import pprint
-
 import shutil
 import django
 
@@ -22,42 +19,53 @@ def create_dir(patern, list_dir):
 patern = "my_project"
 list_dir = ['setting', 'mainapp', 'adminap', 'authapp']
 
-# create_dir(patern, list_dir)
+create_dir(patern, list_dir)
 
 '''
 2* yaml (еще дописываю скрипт)
 '''
 
 
+
+def replaces(str):
+    return str.replace('--', '').replace('|', '').replace('\n', '')
+
+
+list_path = []
 with open('config.yaml') as f:
-    path_yaml = yaml.safe_load(f)
 
-print (path_yaml)
+    for line in f.readlines():
+        if line.find('.') == -1:
+            if line.count('--') == len(list_path):
+                list_path.append(replaces(line))
+                os.makedirs(os.path.join(*list_path), exist_ok=True)
+            else:
+                list_path = list_path[:line.count('--')]
+                list_path.append(replaces(line))
+                os.makedirs(os.path.join(*list_path))
+        else:
+            open(os.path.join(*list_path, replaces(line)), 'w').close()
 
-for key, item in path_yaml.items():
-    print(key)
-    if type(item) == type({}):
-        print(key)
-        print(item)
-        for key_1, item_1 in item.items():
-            if type(item_1) == type({}):
-                print(key_1)
+
 
 
 """
 3. Перенос папок
 """
-#
-# root_dir = os.path.abspath('my project')
-#
-# dst_dir = "templates"
-# for root, dirs, files in os.walk(root_dir):
-#     if dirs == [dst_dir]:
-#         scr_path = os.path.join(root, str(*dirs))
-#         try:
-#             shutil.move(scr_path, root_dir)
-#         except shutil.Error:
-#             copy_tree(scr_path, os.path.join(root_dir, dst_dir))
+
+root_dir = os.path.abspath('my_project')
+
+dst_dir = "templates"
+
+for root, dirs, files in os.walk(root_dir):
+    if dirs == [dst_dir]:
+        print(dirs)
+        scr_path = os.path.join(root, str(*dirs))
+        try:
+            shutil.move(scr_path, root_dir)
+        except shutil.Error:
+            copy_tree(scr_path, os.path.join(root_dir, dst_dir))
+            shutil.rmtree(scr_path)
 
 """
 4 и 5 . {размер кратный 10: (количество файлов, [разрешения файлов]}
@@ -75,12 +83,12 @@ for root, dirs, files in os.walk(root_dir):
         size_file_dict[size_abs].append(ext)
 
 
-# сортировка по количеству файлов
-# for ext, files in sorted(size_file_dict.items(), key=lambda x: len(x[1]), reverse=True):
-    # print(f'{ext}: {len(files), list(set(files))}')
+сортировка по количеству файлов
+for ext, files in sorted(size_file_dict.items(), key=lambda x: len(x[1]), reverse=True):
+    print(f'{ext}: {len(files), list(set(files))}')
 
 
-# сортировка по объему файлов
-# for ext, files in sorted(size_file_dict.items()):
-     # print(f'{ext}: {len(files), list(set(files))}')
+сортировка по объему файлов
+for ext, files in sorted(size_file_dict.items()):
+     print(f'{ext}: {len(files), list(set(files))}')
 
